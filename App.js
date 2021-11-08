8cc086ee75ae129fc80cd4da6e8c76de5826e683
Luiz Sutil <luizsutil@Luizs-MacBook-Air.local>
2021-11-08 00:45:33 -0300

    mds é 15 pra 1

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {View, Text, StyleSheet, TextInput, TouchableOpacity} from 'react-native'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export const BMI = () => {
+
+    const [weight, onChangeWeight] = React.useState("");
+    const [height, onChangeHeight] = React.useState("");
+    const [bmi, onChangeBmi] = React.useState("");
+
+
+    const HandleClick = (ht, wt) => {
+      let _height = ht.replace(/[^0-9]/g, '') / 100
+      let _weight = wt.replace(/[^0-9]/g, '')
+
+      let _bmi = Math.round(_weight/ (_height * _height) * 10) / 10
+      onChangeBmi(_bmi)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text} >Calculador IMC Luiz Sutil</Text>
+
+            <View style={{marginBottom: 55}}>
+                <View style={{marginBottom:20, display:"flex", flexDirection:"row", alignItems:"center"}}>
+                    <TextInput 
+                      style={styles.input}
+                      value={weight}
+                      placeholder='Peso'
+                      onChangeText={onChangeWeight}
+                      keyboardType = 'number-pad'
+                    ></TextInput>
+                    <Text>kg</Text>
+                 </View>
+                <View style={{marginBottom:20, display:"flex", flexDirection:"row", alignItems:"center"}}>
+                  <TextInput 
+                    style={styles.input}
+                    value={height} 
+                    onChangeText={onChangeHeight}
+                    placeholder='Altura'
+                    keyboardType = 'number-pad'
+                  ></TextInput>
+                  <Text>cm</Text>
+                </View>
+            </View> 
+
+            <View style={{marginBottom:30}}>
+              <TouchableOpacity onPress={()=>HandleClick(height,weight)}>
+                  <View style={styles.button}>
+                      <Text style={styles.buttonText}>Calcular</Text>
+                  </View>
+              </TouchableOpacity>
+            </View>
+            <View>
+              {bmi ? <Text style={styles.responseText}>Seu IMC é: {bmi}</Text>: null}
+              {bmi ? <Text style={styles.statusText}>Isto é considerado 
+                {
+                  bmi < 18.5 ? ' abaixo do peso' :
+                  bmi <= 24.9 ? ' ok' :
+                  bmi <= 29.9 ? ' acima do peso' :
+                  ' Obeso'
+                }
+              </Text> : null}
+            </View>
+
+        </View>
+    )
 }
+
+export default BMI
+
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      flex:1,
+      //justifyContent:'center'
+      alignItems: 'center',
+      marginTop: 100 
   },
-});
+  text: {
+      color: '#A86916',
+      fontSize: 30,
+      marginBottom:140
+  },
+  input: {
+      height: 50,
+      margin: 12,
+      width: 70,
+      borderBottomWidth:1,
+      fontSize: 25
+    },
+    button: {
+      borderRadius:8,
+      borderColor:'black',
+      backgroundColor: 'white',
+      width:255,
+      height:50,
+      borderWidth: 1,
+      display:'flex', 
+      alignItems: 'center',
+      justifyContent:'center',
+      
+  },
+  buttonText:{
+      color:'#A86916',
+      fontSize:33,
+  },
+  responseText:{
+    color:'#A86916',
+    fontSize:20,
+  },
+  statusText:{
+  color:'teal',
+  fontSize:20,
+  },
+})
